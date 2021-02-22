c2b45741b6cad2d173b335e2ebd283c015d92e1d
Michael Eschbacher <michael.eschbacher@gmail.com>
2021-02-22 12:58:33 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <w-app style="margin:0 auto; max-width:800px; background-color:whitesmoke; padding:20px;" class="pa4">
+
+        <w-form @submit="fetchInfo">
+            <w-input label="Ticker" :validators="[validators.required]" v-model="ticker"/>
+
+            <w-select label="Property" v-model="lookup" :validators="[validators.required]" :items="lookup_options">
+
+            </w-select>
+            <w-button type="Submit">Submit</w-button>
+        </w-form>
+
+        <div v-show="fetchedData.length" style="margin-top:20px;">
+
+        <pre>
+
+        {{ fetchedData }}
+        </pre>
+        </div>
+    </w-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import {Component, Vue} from 'vue-property-decorator';
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+    components: {},
 })
-export default class App extends Vue {}
-</script>
+export default class App extends Vue {
+    lookup_options = [
+        {value: 'revenue', label: 'Revenue'},
+        {value: 'total-provision-income-taxes', label: 'Total Provision Income Taxes'},
+    ];
+    lookup: string;
+    ticker: string;
+    fetchedData = [];
+    validators = {
+        required: value => !!value || 'This field is required'
+    }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    constructor() {
+        super();
+        this.lookup = 'revenue';
+        this.ticker = 'nflx';
+    }
+
+    async fetchInfo() {
+        console.clear();
+        const resp = await fetch(`/.netlify/functions/hi?ticker=${this.ticker}&property=${this.lookup}`);
+        this.fetchedData = (await resp.json()).data;
+    }
 }
-</style>
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import WaveUI from 'wave-ui';
+import 'wave-ui/dist/wave-ui.css'
 
 Vue.config.productionTip = false
 
+Vue.use(WaveUI);
+
+const waveui = new WaveUI({})
+
 new Vue({
+  waveui,
   render: h => h(App),
 }).$mount('#app')
