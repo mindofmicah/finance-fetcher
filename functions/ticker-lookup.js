e4d735ad0ea3f102f72f182919f0c796eb3bfa84
Michael Eschbacher <michael.eschbacher@gmail.com>
2021-02-24 22:01:42 -0600

    fixed the Netlify config

--- functions/ticker-lookup.js
+++ functions/ticker-lookup.js
@@ -1,25 +1,47 @@
 const fetch = require('node-fetch').default;
+const slugify = require('slugify');
+
+const slugMe = string => {
+    return slugify(string, {
+        replacement: '_',
+        lower: true,
+    })
+};
 const handler = async (event) => {
+
     try {
         const ticker = event.queryStringParameters.ticker;
-        const data = await fetch(`https://www.macrotrends.net/stocks/charts/${ticker}/${ticker}/income-statement`);
-        const body_text = await data.text();
+        const financial_data = {};
+        for (const financial_statement of ['income-statement', 'balance-sheet', 'cash-flow-statement']) {
 
-        const response_data = JSON.parse(body_text.match(/var originalData = (\[.+?\])/)[1]).map((current) => {
-            const {field_name, popup_icon, ...dates} = current;
-            return {
-                label: field_name.match(/>(.+?)</)[1],
-                dates
-            }
-        });
+            const data = await fetch(`https://www.macrotrends.net/stocks/charts/${ticker}/${ticker}/${financial_statement}`);
+            const body_text = await data.text();
+
+            const response_data = JSON.parse(body_text.match(/var originalData = (\[.+?\])/)[1]).reduce((carry, current, index) => {
+                let {field_name, popup_icon, ...dates} = current;
+                let label = field_name.match(/>(.+?)</)[1];
+                carry[slugMe(label)] = ({
+                    position: index,
+                    label,
+                    values: Object.entries(dates).map(data => {
+                        const [date, value] = data;
+                        return {date, value};
+                    })
+                });
+                return carry;
+            }, {});
+            financial_data[slugMe(financial_statement, '_')] = response_data;
+
+        }
 
         return {
             statusCode: 200,
             body: JSON.stringify({
-                data: response_data
-            })
+                financial_data
+
+            }),
             // // more keys you can return:
-            // headers: { "headerName": "headerValue", ... },
+            headers: {"Content-Type": "application/json"},
             // isBase64Encoded: true,
         }
     } catch (error) {
