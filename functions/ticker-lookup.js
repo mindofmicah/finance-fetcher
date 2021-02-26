883b449dd429669604a064795c0af964fff9d857
Michael Eschbacher <michael.eschbacher@gmail.com>
2021-02-25 20:46:59 -0600

    slim down the API request

--- functions/ticker-lookup.js
+++ functions/ticker-lookup.js
@@ -1,44 +1,44 @@
 const fetch = require('node-fetch').default;
-const slugify = require('slugify');
 
-const slugMe = string => {
-    return slugify(string, {
-        replacement: '_',
-        lower: true,
-    }).replace(/-/g, '_')
-};
 const handler = async (event) => {
-
     try {
         const ticker = event.queryStringParameters.ticker;
-        const financial_data = {};
-        for (const financial_statement of ['income-statement', 'balance-sheet', 'cash-flow-statement']) {
+        const financial_statements = [];
+        const statements = [
+            {name: 'Income Statement', slug: 'income-statement'},
+            {name: 'Balance Sheet', slug: 'balance-sheet'},
+            {name: 'Cash Flow Statement', slug: 'cash-flow-statement'},
+        ];
+        for (const financial_statement of statements) {
 
-            const data = await fetch(`https://www.macrotrends.net/stocks/charts/${ticker}/${ticker}/${financial_statement}`);
+            const data = await fetch(`https://www.macrotrends.net/stocks/charts/${ticker}/${ticker}/${financial_statement.slug}`);
             const body_text = await data.text();
 
-            const response_data = JSON.parse(body_text.match(/var originalData = (\[.+?\])/)[1]).reduce((carry, current, index) => {
+            const statement_data = {
+                name: financial_statement.name,
+            };
+            statement_data.items = JSON.parse(body_text.match(/var originalData = (\[.+?\])/)[1]).map(current => {
                 let {field_name, popup_icon, ...dates} = current;
-                let label = field_name.match(/>(.+?)</)[1];
-                carry[slugMe(label)] = ({
-                    position: index,
-                    label,
+                let name = field_name.match(/>(.+?)</)[1];
+                return {
+                    name,
                     values: Object.entries(dates).map(data => {
                         const [date, value] = data;
                         return {date, value};
                     })
-                });
-                return carry;
+                }
             }, {});
-            financial_data[slugMe(financial_statement, '_')] = response_data;
+
+            financial_statements.push(statement_data);
 
         }
 
         return {
             statusCode: 200,
             body: JSON.stringify({
-                financial_data
-
+                data: {
+                    financial_statements
+                }
             }),
             // // more keys you can return:
             headers: {"Content-Type": "application/json"},
